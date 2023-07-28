fb4d18d687e6a78f70677614342200c1416965c9
Sahil Mund <130903382+sahil-009@users.noreply.github.com>
2023-07-28 18:52:41 +0530

    need internet connection for showing icons
    using html,css and javascript

---
+++ spotify-clone/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+// Handle play/pause click
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-play-circle");
+    masterPlay.classList.add("fa-pause-circle");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-pause-circle");
+    masterPlay.classList.add("fa-play-circle");
+    gif.style.opacity = 0;
+  }
